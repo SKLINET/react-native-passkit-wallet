c6a9f2e44b41489af1378e1a3c97b23dcd47a9bc
Matěj Nevlud <matej.nevlud@le.cz>
2021-06-29 16:25:14 +0200

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ import { EmitterSubscription, ViewProperties } from 'react-native';
 declare class PassKit {
   static canAddPasses(): Promise<boolean>;
   static addPass(base64Encoded: string, fileProvider?: string): Promise<void>;
+  static addPasses(arrayOfBase64Encoded: Array, fileProvider?: string): Promise<void>;
   static presentAddPassesViewController(base64Encoded: string): Promise<void>;
   static addEventListener(
     eventType: string,
